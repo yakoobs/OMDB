e079175756f63eb358eca05da99c8e940dcf633e
Jakub Sokołowski <jakub.m.sokolowski@gmail.com>
2021-02-15 21:51:45 +0100

    Initial implementation for API methods.

--- OMDB browser/OMDB browser/API/MoviesDatabaseAPI.swift
+++ OMDB browser/OMDB browser/API/MoviesDatabaseAPI.swift
@@ -7,17 +7,43 @@
 
 import Foundation
 
+typealias SearchResultCompletion = (Result<OMDBSearchResult,Error>) -> Void
+typealias FetchDetailsCompletion = (Result<MovieDetails,Error>) -> Void
+
 protocol MoviesDatabaseAPIProtocol {
-    func fetchResults(query: String)
-    func fetchDetails(movieId: String)
+    func search(query: String, completion: @escaping SearchResultCompletion)
+    func fetchDetails(movieId: String, completion: @escaping FetchDetailsCompletion)
 }
 
 final class MovieDatabaseAPI: MoviesDatabaseAPIProtocol {
-    func fetchResults(query: String) {
-        let url = "http://www.omdbapi.com/?apikey=b9bd48a6&s=\(query)&type=movie"
+    func search(query: String, completion: @escaping SearchResultCompletion) {
+        
+        if let url = APIEndpoints.search(query).url {
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                if let data = data {
+                    do {
+                        let result = try JSONDecoder().decode(OMDBSearchResult.self, from: data)
+                        completion(.success(result))
+                    } catch let error {
+                        completion(.failure(error))
+                    }
+                }
+            }.resume()
+        }
     }
     
-    func fetchDetails(movieId: String) {
-        let url = "http://www.omdbapi.com/?apikey=b9bd48a6&i=\(movieId)"
+    func fetchDetails(movieId: String, completion: @escaping FetchDetailsCompletion) {
+        if let url = APIEndpoints.details(movieId).url {
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                if let data = data {
+                    do {
+                        let details = try JSONDecoder().decode(MovieDetails.self, from: data)
+                        completion(.success(details))
+                    } catch let error {
+                        completion(.failure(error))
+                    }
+                }
+            }.resume()
+        }
     }
 }
---
+++ OMDB browser/OMDB browser/API/URLs.swift
@@ -0,0 +1,31 @@
+//
+//  URLs.swift
+//  OMDB browser
+//
+//  Created by Sokołowski Jakub (FIRMA ZEWNĘTRZNA ) on 15/02/2021.
+//
+
+import Foundation
+
+enum APIEndpoints {
+    case search(String)
+    case details(String)
+    
+    var url: URL? {
+        return URL(string: urlString)
+    }
+    
+    private var urlString: String {
+        switch self {
+        case .search(let query):
+            return APIConstatnts.baseURL + "?apikey=" + APIConstatnts.apiKey + "&s=" + query + "&type=movie"
+        case .details(let movieId):
+            return APIConstatnts.baseURL + "?apikey=" + APIConstatnts.apiKey + "&i=" + movieId
+        }
+    }
+}
+
+struct APIConstatnts {
+    static let baseURL = "http://www.omdbapi.com/"
+    static let apiKey = "b9bd48a6"
+}
